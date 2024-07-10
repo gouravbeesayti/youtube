60e78f9215d5b5fd319db9ba8ca75019aa93021d
gouravbeesayti <gbeesayti@gmail.com>
2024-07-10 17:51:38 +0530

    some changes in utils of apiResponse file

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
@@ -18,19 +19,28 @@ const videoSchema = new Schema(
             type:String, 
             required:true,
         },
-        time:{
-            type:Number, 
-            required:true,
-        },
         duration:{
             type:Number, 
             required:true,
         },
+        views:{
+            type:Number,
+            default:0
+        },
+        isPublished:{
+            type:Boolean,
+            default:true
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        }
  
     },
     {
         timestamps:true
     }
 )
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video",videoSchema)
